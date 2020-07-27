4332aefafb79eaf5ce6e77f98542e11cfbc7ec0b
Christian Mayer <christian@fox21.at>
2020-07-27 23:15:35 +0200

    Byte-range download.

--- playlist_download.sh
+++ playlist_download.sh
@@ -19,9 +19,15 @@ function download_ts_file() {
 	local url="$1"
 	local url_basename=$(basename "$url")
 	local tmp_file="${url_basename}.tmp"
+	local file_number="$2"
+	#local file_number="$2"
+
+	if [[ "$url_basename" = media.ts ]] ; then
+		url_basename=$(printf "media_%06d.ts" $file_number)
+	fi
+
 	if [[ ! -f "$url_basename" ]]; then
-		echo -e "${GREEN} -> download ts file: '$url'${NO_COLOR}"
-		#cp "$url" "$tmp_file"
+		echo -e "${GREEN} -> download ts file #${file_number}: '$url'${NO_COLOR}"
 		wget -O "$tmp_file" "$url"
 		mv "$tmp_file" "$url_basename"
 	else
@@ -56,48 +62,54 @@ function download_playlist() {
 		exit 1
 	fi
 	
-	# echo "pl_file2: $pl_file2"
-	# echo '----- pl2 -----'
-	# head -10 "$pl_file2"
-	# echo '---------------'
-	# sleep 1
-	
 	local m3u_file1=$(mktemp "$tmp_dir/m3u1.XXXXXXXXXXXXX")
-	local ts_file1=$(mktemp "$tmp_dir/ts1.XXXXXXXXXXXXX")
-	
 	grep .m3u8 "$pl_file2" > "$m3u_file1"
-	grep .ts "$pl_file2" > "$ts_file1"
-	
-	# echo "m3u_file1: $m3u_file1"
-	# echo '----- m3u file -----'
-	# head -10 "$m3u_file1"
-	# echo '--------------------'
-	# sleep 1
-	
-	# echo "ts_file1: $ts_file1"
-	# echo '----- ts file -----'
-	# tail -10 "$ts_file1"
-	# echo '-------------------'
-	# sleep 1
-	
-	first_ts_file=$(head -1 "$ts_file1")
-	last_ts_file=$(tail -1 "$ts_file1")
-	
 	while read -r line ; do
 		echo " -> m3u line: $line"
-		download_playlist "$tmp_dir" "${url_dirname}/${line}"
+		#download_playlist "$tmp_dir" "${url_dirname}/${line}"
 		sleep 0.3
 	done < "$m3u_file1"
 	
-	while read -r line ; do
-		echo " -> ts line: $line/$last_ts_file"
-		download_ts_file "${url_dirname}/${line}"
-		sleep 0.3
-	done < "$ts_file1"
+	local ts_file1=$(mktemp "$tmp_dir/ts1.XXXXXXXXXXXXX")
+	grep .ts "$pl_file2" > "$ts_file1"
 	
-	if [[ $(wc -l "$ts_file1" | awk '{ print $1 }') -gt 1 ]]; then
-		echo -e "${YELLOW} -> first ts file: $first_ts_file${NO_COLOR}"
-		echo -e "${YELLOW} -> last  ts file: $last_ts_file${NO_COLOR}"
+	if [[ $(uniq -c "$ts_file1" | awk '{ print $1 }') -gt 1 ]] ; then
+		echo "-> GREP"
+		#local br_file1=$(mktemp "$tmp_dir/br1.XXXXXXXXXXXXX")
+		local br_file1=br1.tmp
+		grep -A 1 EXT-X-BYTERANGE "$pl_file1" > "$br_file1"
+		echo "-> byte-range feil: ${br_file1}"
+		split -l 300 "$br_file1" br1.part1_xxxx
+		for split_file in ./br1.part1_* ; do
+			echo "-> split file: ${split_file}"
+			split -l 3 "$split_file" ${split_file}.part2_xxxxxx
+			rm "${split_file}"
+		done
+
+		for split_file in ./br1*part2_* ; do
+			#echo "-> split file B: ${split_file}"
+			br_infos=$(grep EXT-X-BYTERANGE "${split_file}" | sed 's/#EXT-X-BYTERANGE://; s/@/ /')
+			br_len=$(printf %d $br_infos)
+			br_pos=$(echo $br_infos | awk '{ print $2 }')
+			echo "-> byte-range len: '${br_len}'"
+			echo "-> byte-range pos: '${br_pos}'"
+			rm "${split_file}"
+		done
+	else
+		first_ts_file=$(head -1 "$ts_file1")
+		last_ts_file=$(tail -1 "$ts_file1")
+		line_number=0
+		while read -r line ; do
+			echo " -> ts line #${line_number}: $line/$last_ts_file"
+			#download_ts_file "${url_dirname}/${line}" $line_number
+			#sleep 0.3
+			let "line_number += 1"
+		done < "$ts_file1"
+		
+		if [[ $(wc -l "$ts_file1" | awk '{ print $1 }') -gt 1 ]]; then
+			echo -e "${YELLOW} -> first ts file: $first_ts_file${NO_COLOR}"
+			echo -e "${YELLOW} -> last  ts file: $last_ts_file${NO_COLOR}"
+		fi
 	fi
 }
 
@@ -123,6 +135,7 @@ tmp_dir=$(mktemp -d -t 'playlist_downloader')
 echo "tmp dir: $tmp_dir"
 
 download_playlist "$tmp_dir" "$url"
+#youtube-dl "$url"
 
 echo 'clean up'
 rm -rf "$tmp_dir"
